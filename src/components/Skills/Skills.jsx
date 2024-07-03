97866998dc3a7254194cd0de8451dc6a7d76efb4
João Ricardo Fortunato <ricardo.forttunato@gmail.com>
2024-07-03 11:01:24 -0300

    Criando timeLine para conteudo da aba Skills

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -1,88 +1,90 @@
 import styles from "./Skills.module.css";
-import bannerFront from "/image/background-banner.png";
+import TimeLine from "../TimeLine/TimeLine";
+import { content2022, content2023, content2024 } from "../../TimeLineContent";
 
-import { IoSchool } from "react-icons/io5" ;
-import { FaReact } from "react-icons/fa6";
-import { FaHtml5 } from "react-icons/fa6";
-import { IoLogoCss3 } from "react-icons/io";
-import { IoLogoJavascript } from "react-icons/io5";
-import { FaGithub } from "react-icons/fa6";
-import { SiUdemy } from "react-icons/si";
-import { SiCisco } from "react-icons/si";
-import { LiaFreeCodeCamp } from "react-icons/lia";
-import { SiPython } from "react-icons/si";
-import { SiCsharp } from "react-icons/si";
-import { FaNode } from "react-icons/fa";
-import { FaBootstrap } from "react-icons/fa6";
-import { IoLogoSass } from "react-icons/io";
-import { FaGitAlt } from "react-icons/fa";
-import { VscVscode } from "react-icons/vsc";
+export default function Skills() { 
 
-export default function Skills() {
+  function handleContent(content) {
+    return (
+      <TimeLine 
+        key={content.id}
+        title={content.title}
+        icon1={content.icon1}
+        icon2={content.icon2}
+        icon3={content.icon3}
+        icon4={content.icon4}
+        icon5={content.icon5}
+        icon6={content.icon6}
+        icon7={content.icon7}
+        icon8={content.icon8}
+        icon9={content.icon9}
+
+        certification={content.certificationUrl}
+      />
+    )
+  }
+
   return (
     <main className={styles.skills}>
-      <div className={styles.year}>
 
-      </div>
+      <h1 className={styles.title}>Skills Learning Timeline</h1>
+      <div className={styles.container}>
 
-      <div className={styles.moving_banner}>
-        <img className={styles.banner} src={bannerFront} alt="" />
+        <div className={styles.skills_container}>
+          <div className={styles.year_container}>
+            <div className={styles.year_title}>2022</div>
+          </div>
+          <div className={styles.line_container}>
+            <div className={styles.line_left_container}>
+              <div className={styles.left_line}></div>
+            </div>
+            <div className={styles.polygon}></div>
+            <div className={styles.line_right_container}>
+              <div className={styles.right_line}></div>
+            </div>
+          </div>
+          <div className={styles.content}>
+            {content2022.map(handleContent)}
+          </div>
+        </div>
+          {/* -------------------------------------------------------- */}
+        <div className={styles.skills_container}>
+          <div className={styles.content_reverse}>
+            {content2023.map(handleContent)}
+          </div>
+          <div className={styles.line_container_reverse}>
+            <div className={`${styles.line_left_container_reverse} ${styles.eixo_colorLight}`}>
+              <div className={`${styles.left_line_reverse} ${styles.line_colorLight}`}></div>
+            </div>
+            <div className={styles.polygon_reverse}></div>
+            <div className={`${styles.line_right_container_reverse} ${styles.eixo_colorLight}`}>
+              <div className={`${styles.right_line_reverse} ${styles.line_colorLight}`}></div>
+            </div>
+          </div>
+          <div className={styles.year_container_reverse}>
+            <div className={`${styles.year_title_reverse} ${styles.title_colorLight}`}>2023</div>
+          </div>
+        </div>
+          {/* ---------------------------------------------------------------- */}
+        <div className={styles.skills_container}>
+          <div className={styles.year_container}>
+            <div className={`${styles.year_title} ${styles.title_colorDark}`}>2024</div>
+          </div>
+          <div className={styles.line_container}>
+            <div className={`${styles.line_left_container} ${styles.eixo_colorDark}`}>
+              <div className={`${styles.left_line} ${styles.line_colorDark}`}></div>
+            </div>
+            <div className={styles.polygon_colorDark}></div>
+            <div className={`${styles.line_right_container} ${styles.eixo_colorDark}`} >
+              <div className={`${styles.right_line} ${styles.line_colorDark}`}></div>
+            </div>
+            <div className={styles.circle}></div>
+          </div>
+          <div className={styles.content}>
+            {content2024.map(handleContent)}
+          </div>
+        </div>
       </div>
-
-      <div className={styles.school}>
-        <IoSchool className={styles.icon} />
-      </div>
-      <div className={styles.udemy}>
-        <SiUdemy className={styles.icon} />
-      </div>
-      <div className={styles.cisco}>
-        <SiCisco className={styles.icon} />
-      </div>
-      <div className={styles.freeCodeCamp}>
-        <LiaFreeCodeCamp className={styles.icon} />
-      </div>
-
-      <div className={styles.html}>
-        <FaHtml5 className={styles.icon} />
-      </div>
-      <div className={styles.css}>
-        <IoLogoCss3 className={styles.icon} />
-      </div>
-      <div className={styles.javascript}>
-        <IoLogoJavascript className={styles.icon} />
-      </div>
-
-      <div className={styles.react}>
-        <FaNode className={styles.icon} />
-      </div>
-      <div className={styles.react}>
-        <SiPython className={styles.icon} />
-      </div>
-      <div className={styles.react}>
-        <SiCsharp className={styles.icon} />
-      </div>
-
-
-      <div className={styles.react}>
-        <FaReact className={styles.icon} />
-      </div>
-      <div className={styles.react}>
-        <FaBootstrap className={styles.icon} />
-      </div>
-      <div className={styles.react}>
-        <IoLogoSass className={styles.icon} />
-      </div>
-
-      <div className={styles.react}>
-        <FaGitAlt className={styles.icon} />
-      </div>
-      <div className={styles.react}>
-        <FaGithub className={styles.icon} />
-      </div>
-      <div className={styles.react}>
-        <VscVscode className={styles.icon} />
-      </div>
-
     </main>
   );
 }
