fba11e2db3001a0eca77860ecee49558806948c4
João Ricardo Fortunato <ricardo.forttunato@gmail.com>
2024-07-03 11:08:11 -0300

    Criado arquivo de dados sobre cursos e instituições, deletado img desncessaria paraa o projeto

---
+++ src/TimeLineContent.jsx
@@ -0,0 +1,177 @@
+import { IoSchool } from "react-icons/io5";
+import { FaReact } from "react-icons/fa6";
+import { FaHtml5 } from "react-icons/fa6";
+import { IoLogoCss3 } from "react-icons/io";
+import { IoLogoJavascript } from "react-icons/io5";
+import { FaGithub } from "react-icons/fa6";
+import { SiUdemy } from "react-icons/si";
+import { SiCisco } from "react-icons/si";
+import { LiaFreeCodeCamp } from "react-icons/lia";
+import { SiPython } from "react-icons/si";
+import { SiCsharp } from "react-icons/si";
+import { FaNode } from "react-icons/fa";
+import { FaBootstrap } from "react-icons/fa6";
+import { IoLogoSass } from "react-icons/io";
+import { FaGitAlt } from "react-icons/fa";
+import { FaLaptopCode } from "react-icons/fa";
+import { TbCertificate } from "react-icons/tb";
+import { TbCertificateOff } from "react-icons/tb";
+import { FaJava } from "react-icons/fa6";
+import { GrMysql } from "react-icons/gr";
+import { PiFileSql } from "react-icons/pi";
+
+const content2022 = [
+  {
+    id: 1,
+    title: "Lógica de Programação e Algoritmos",
+    icon1: <SiUdemy />,
+    icon2: <TbCertificate />,
+    certificationUrl:
+      "https://www.udemy.com/certificate/UC-e072e5b4-3c1d-45a4-9813-f83b4c27df46/",
+    icon3: <SiCsharp />,
+    icon4: <SiPython />,
+    icon5: <FaJava />,
+    icon6: null,
+    icon7: null,
+  },
+  {
+    id: 2,
+    title: "C# e Lógica de Programação e Algoritmos",
+    icon1: <SiUdemy />,
+    icon2: <TbCertificate />,
+    certificationUrl:
+      "https://www.udemy.com/certificate/UC-21b73499-eb8b-49d4-b829-ee3266da0b45/",
+    icon3: <SiCsharp />,
+    icon4: null,
+    icon5: null,
+    icon6: null,
+    icon7: null,
+  },
+];
+
+const content2023 = [
+  {
+    id: 5,
+    title: "Responsive Web Design",
+    icon1: <LiaFreeCodeCamp />,
+    icon2: <TbCertificate />,
+    certificationUrl:
+      "https://www.freecodecamp.org/certification/fcca6cff155-5a32-40b1-827d-88ffcb2c099a/responsive-web-design",
+    icon3: <FaHtml5 />,
+    icon4: <IoLogoCss3 />,
+    icon5: null,
+    icon6: null,
+    icon7: null,
+  },
+  {
+    id: 4,
+    title: "Git e Github Essencial para Desenvolvedores",
+    icon1: <SiUdemy />,
+    icon2: <TbCertificate />,
+    certificationUrl:
+      "https://www.udemy.com/certificate/UC-6ae08abf-6768-48b8-90ff-822cd277b99b/",
+    icon3: <FaGithub />,
+    icon4: <FaGitAlt />,
+    icon5: null,
+    icon6: null,
+    icon7: null,
+  },
+  {
+    id: 3,
+    title: "Python Essentials",
+    icon1: <SiCisco />,
+    icon2: <TbCertificate />,
+    certificationUrl:
+      "https://www.credly.com/badges/ff6d97c3-2d7a-4325-8d5f-677aefc4885a/",
+    icon3: <SiPython />,
+    icon4: null,
+    icon5: null,
+    icon6: null,
+    icon7: null,
+  },
+  {
+    id: 2,
+    title: "IT Essentials",
+    icon1: <SiCisco />,
+    icon2: <TbCertificate />,
+    certificationUrl:
+      "https://www.credly.com/badges/a8c0be4d-218d-4f69-83d6-e9c7a7c233d3/linked_in_profile",
+    icon3: <FaLaptopCode />,
+    icon4: null,
+    icon5: null,
+    icon6: null,
+    icon7: null,
+  },
+  {
+    id: 1,
+    title: "Analise e Desenvolvimento de Sistemas",
+    icon1: <IoSchool />,
+    icon2: <TbCertificateOff />,
+    certificationUrl: null,
+    icon3: <SiCsharp />,
+    icon4: <GrMysql />,
+    icon5: <FaLaptopCode />,
+    icon6: null,
+    icon7: null,
+  },
+];
+
+const content2024 = [
+  {
+    id: 1,
+    title: "ONE-OracleNextGeneration Front-end Developer",
+    icon1: <IoSchool />,
+    icon2: <TbCertificate />,
+    certificationUrl:
+      "https://cursos.alura.com.br/user/ricardo-forttunato/fullCertificate/4022aadb4953d9ce991c115dcd2e033d",
+    icon3: <FaHtml5 />,
+    icon4: <IoLogoCss3 />,
+    icon5: <IoLogoJavascript />,
+    icon6: <FaGitAlt />,
+    icon7: <FaGithub />,
+    icon8: <FaReact />,
+  },
+  {
+    id: 2,
+    title: "Desenvolvimento Orientado a Objetos com Python",
+    icon1: <IoSchool />,
+    icon2: <TbCertificate />,
+    certificationUrl:
+      "https://www.linkedin.com/in/ricardo-fortunato/details/certifications/1710342741514/single-media-viewer/?profileId=ACoAAES_hi8BuSAn0DpVSa5lk6S5g19qjsU3vFc",
+    icon3: <SiPython />,
+    icon4: null,
+    icon5: null,
+    icon6: null,
+    icon7: null,
+  },
+  {
+    id: 3,
+    title: "The Complete Web Development Bootcamp",
+    icon1: <SiUdemy />,
+    icon2: <TbCertificate />,
+    certificationUrl:
+      "https://www.udemy.com/certificate/UC-b5f5fbc0-4b15-4577-a64e-0ff1b92eec4b/",
+    icon3: <FaHtml5 />,
+    icon4: <IoLogoCss3 />,
+    icon5: <FaBootstrap />,
+    icon6: <IoLogoJavascript />,
+    icon7: <FaNode />,
+    icon8: <PiFileSql />,
+    icon9: <FaReact />,
+  },
+  {
+    id: 4,
+    title: "Trilha Digital Back End - Coders 2024",
+    icon1: <IoSchool />,
+    icon2: <TbCertificate />,
+    certificationUrl:
+      "https://ada.tech/certificado?code=f09453f2-c8a8-a6ee-dfa0-21045d1344d1",
+    icon3: <FaJava />,
+    icon4: <FaGithub />,
+    icon5: <FaGitAlt />,
+    icon6: <FaLaptopCode />,
+    icon7: null,
+  },
+];
+
+export { content2022, content2023, content2024 };
