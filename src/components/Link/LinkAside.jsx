42041cc0b9c12bd9ad64777f6081b1050525aa9e
João Ricardo Fortunato <ricardo.forttunato@gmail.com>
2024-07-03 16:46:07 -0300

    alterando title no index, criando componente LinkAside para mapeamento no componente Sidebar, refatoração do codigo no componente sidebar

---
+++ src/components/Link/LinkAside.jsx
@@ -0,0 +1,34 @@
+import { useEffect, useState } from 'react';
+import styles from './Link.module.css';
+import { Link, useLocation } from "react-router-dom";
+
+/*---- Icons Import ----*/
+
+export default function LinkAside(props) {
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    function handleResize() {
+      setWindowWidth(window.innerWidth);
+    }
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    }
+  });
+
+  const location = useLocation();
+  const path = location.pathname;
+
+  return (
+    <Link
+      to={props.pathLink}
+      className={props.pathLink === path ? styles.active : null}
+    >
+      <div className={styles.icon}>
+        {props.icon}
+        <span className={ windowWidth < 650 ? styles.deactive : null}>{props.text}</span>
+      </div>
+    </Link>
+  );
+}
--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -1,5 +1,6 @@
 import styles from "./SideBar.module.css";
 import avatar from "/image/avatar.jpg";
+import LinkAside from "../Link/LinkAside";
 
 /*---- Icons Import ----*/
 import { GoHomeFill } from "react-icons/go";
@@ -7,62 +8,33 @@ import { IoPersonSharp } from "react-icons/io5";
 import { FaCode } from "react-icons/fa6";
 import { IoMdMail } from "react-icons/io";
 import { BiSolidPhotoAlbum } from "react-icons/bi";
-import { Link, useLocation } from "react-router-dom";
-import { useState, useEffect } from "react";
 
 export default function SideBar() {
 
-  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+  const linkData =[
+    {id: 1, path: "/", icon: <GoHomeFill />, text: "Home"},
+    {id: 2, path: "/about", icon: <IoPersonSharp />, text: "About"},
+    {id: 3, path: "/skills", icon: <FaCode />, text: "Skills"},
+    {id: 4, path: "/portfolio", icon: <BiSolidPhotoAlbum />, text: "Portfolio"},
+    {id: 5, path: "/contact", icon: <IoMdMail />, text: "Contact"}
+  ]
 
-  useEffect(() => {
-    function handleResize() {
-      setWindowWidth(window.innerWidth);
-    }
-
-    window.addEventListener('resize', handleResize);
-
-    return () => {
-      window.removeEventListener('resize', handleResize);
-    }
-  });
-
-	const location = useLocation();
+  function renderLinkAside(item, index) {
+    return (
+      <LinkAside
+        key={index}
+        pathLink={item.path}
+        icon={item.icon}
+        text={item.text}
+      />
+    )
+  }
 
   return (
     <aside className={styles.sideBar}>
       <div className={styles.list}>
         <img src={avatar} alt="avatar" className={styles.avatar} />
-
-        <Link to="/" className={location.pathname === '/'? styles.active : null}>
-          <div className={styles.icon}>
-            <GoHomeFill />
-            <span className={windowWidth < 650 ? styles.deactive : null}>Home</span>
-          </div>
-        </Link>
-        <Link to="/about" className={location.pathname === '/about'? styles.active : null}>
-          <div className={styles.icon} >
-            <IoPersonSharp />
-            <span className={windowWidth < 650 ? styles.deactive : null}>About</span>
-          </div>
-        </Link>
-        <Link to="skills" className={location.pathname === '/skills'? styles.active : null}>
-          <div className={styles.icon} >
-            <FaCode />
-            <span className={windowWidth < 650 ? styles.deactive : null}>Skills</span>
-          </div>
-        </Link>
-        <Link to="portfolio" className={location.pathname === '/portfolio'? styles.active : null}>
-          <div className={styles.icon} >
-            <BiSolidPhotoAlbum />
-            <span className={windowWidth < 650 ? styles.deactive : null}>Portfolio</span>
-          </div>
-        </Link>
-        <Link to="contact" className={location.pathname === '/contact'? styles.active : null}>
-          <div className={styles.icon} >
-            <IoMdMail />
-            <span className={windowWidth < 650 ? styles.deactive : null}>Contact</span>
-          </div>
-        </Link>
+        {linkData.map(renderLinkAside)}
       </div>
     </aside>
   );
