893050ba0ac4f8f752e7269cc1a47b24ea52bfba
Ricardo-Forttunato <ricardo.forttunato@gmail.com>
2026-06-11 11:38:55 -0300

    chore: add setup do silence react-router and i18next warnings into terminal running tests

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,1 +1,36 @@
-import '@testing-library/jest-dom'
+import "@testing-library/jest-dom";
+import { vi } from "vitest";
+
+// Mocking CSS Modules
+
+// Mocking original i18n to avoid issues when link embedded in react-router-dom components
+vi.mock("react-i18next", async (importOriginal) => {
+  const actual = await importOriginal<typeof import("react-i18next")>();
+  return {
+    ...actual,
+    useTranslation: () => ({
+      t: (key: string) => key,
+      i18n: {
+        resolvedLanguage: "pt",
+      },
+    }),
+  };
+});
+
+// Mocking React Router DOM to avoid issues with components that use Link or NavLink
+vi.mock("react-router-dom", async (importOriginal) => {
+  const actual = await importOriginal<typeof import("react-router-dom")>();
+
+  return {
+    ...actual,
+    BrowserRouter: ({ children }: { children: React.ReactNode }) => {
+      return actual.BrowserRouter({
+        future: {
+          v7_startTransition: true,
+          v7_relativeSplatPath: true,
+        },
+        children,
+      });
+    },
+  };
+});
